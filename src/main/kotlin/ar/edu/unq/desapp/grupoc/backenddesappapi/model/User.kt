344521e86d32fa59a209b1c4aab6cad3f6e281b5
dkippes <kippes.diego@gmail.com>
2024-03-27 20:38:19 -0300

    se mueve carpeta

---
+++ src/main/kotlin/ar/edu/unq/desapp/grupoc/backenddesappapi/model/User.kt
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoc.backenddesappapi.model
+
+class User {
+var id: Long? = null
+    var name: String? = null
+    var lastName: String? = null
+    var email: String? = null
+    var password: String? = null
+    var cvu: String? = null
+    var address: String? = null
+    var walletAddress: String? = null
+    var reputation: Int? = null
+}
