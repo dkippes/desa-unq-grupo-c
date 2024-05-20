37132c742765b7123b30f9c595e87453f50a700b
dkippes <kippes.diego@gmail.com>
2024-05-20 00:51:30 -0300

    fix currencyTest

--- src/test/kotlin/ar/edu/unq/desapp/grupoc/backenddesappapi/webservice/CryptoControllerTest.kt
+++ src/test/kotlin/ar/edu/unq/desapp/grupoc/backenddesappapi/webservice/CryptoControllerTest.kt
@@ -1,22 +1,27 @@
 package ar.edu.unq.desapp.grupoc.backenddesappapi.webservice
 
 import ar.edu.unq.desapp.grupoc.backenddesappapi.configuration.JacksonConfig
+import ar.edu.unq.desapp.grupoc.backenddesappapi.model.CryptoCurrency
+import ar.edu.unq.desapp.grupoc.backenddesappapi.model.CryptoCurrencyList
 import ar.edu.unq.desapp.grupoc.backenddesappapi.model.enums.SYMBOL
 import ar.edu.unq.desapp.grupoc.backenddesappapi.service.impl.CryptoService
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.jayway.jsonpath.JsonPath
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.annotation.Import
 import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
-import com.jayway.jsonpath.JsonPath
-import org.junit.jupiter.api.Assertions
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.math.BigDecimal
+import java.time.LocalDateTime
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
@@ -26,14 +31,15 @@ class CryptoControllerTest {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
-    @Autowired
+    @MockBean
     private lateinit var cryptoService: CryptoService
-
-    @Autowired
-    private lateinit var objectMapper: ObjectMapper
 
     @Test
     fun shouldReturnAllCryptoCurrencies() {
+        val emptyList  = CryptoCurrencyList(emptyList())
+        Mockito.`when`(cryptoService.getAllCryptoCurrencyPrices())
+            .thenReturn(emptyList)
+
         mockMvc.perform(MockMvcRequestBuilders.get("/crypto/currencies"))
             .andExpect(MockMvcResultMatchers.status().isOk)
             .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
@@ -42,6 +48,9 @@ class CryptoControllerTest {
     @Test
     fun shouldReturnCryptoCurrencyBySymbol() {
         val symbol = SYMBOL.BNBUSDT
+        val crypto  = CryptoCurrency(symbol, BigDecimal.TEN, LocalDateTime.now())
+        Mockito.`when`(cryptoService.getCryptoCurrencyPrice(symbol))
+            .thenReturn(crypto)
 
         val result = mockMvc.perform(MockMvcRequestBuilders.get("/crypto/currency/$symbol"))
             .andExpect(MockMvcResultMatchers.status().isOk)
