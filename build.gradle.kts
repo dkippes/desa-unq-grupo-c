51fd25086c0f2ae6cb5b19ad5a75d2920be7a7ed
dkippes <kippes.diego@gmail.com>
2024-06-05 02:12:47 -0300

    exclude handler

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ sonar {
         property("sonar.organization", "dkippes")
         property("sonar.host.url", "https://sonarcloud.io")
         property("sonar.coverage.jacoco.xmlReportPaths", "$buildDir\\reports\\jacoco\\test\\jacocoTestReport.xml")
-        property("sonar.coverage.exclusions", "**/configuration/**")
+        property("sonar.coverage.exclusions", "**/configuration/**,ar/edu/unq/desapp/grupoc/backenddesappapi/webservice/handlers/**")
     }
 }
 
